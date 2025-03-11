5defa240073ace940c366a9c05dc87a134e036db
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-03-11 08:25:23 +0100

    practical4

---
+++ practical4/variables.py
@@ -0,0 +1,13 @@
+a = 15 #the time they use by walking to the bus station
+b = 75 #the time (in minutes) of bus ride
+c = a + b #total time of the first way
+
+d = 90 # the time (in minutes) of car ride
+e = 5 #the time they use walking from parking lot to the company
+f = d + e #total time of the second way
+
+print(c,f)
+'''
+because c=90<f=95
+the first way is faster than the second
+'''
