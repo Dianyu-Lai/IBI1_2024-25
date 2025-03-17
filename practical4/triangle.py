2fad0691e6c5edc9244ba0669c61795646974979
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-03-17 15:15:59 +0800

    change

--- practical4/triangle.py
+++ practical4/triangle.py
@@ -1,14 +1,13 @@
 a=0
-b=1
 for i in range(1,11):
-    a=a+b
-    b+=1
+    a+=i
+    i+=1
     print(a)
 """
 BEGIN
     SET a = 0
     SET b = 1
-    FOR i FROM 1 TO 10 DO
+    FOR i FROM 1 TO 11 DO
         a = a + b
         b = b + 1
         PRINT a
