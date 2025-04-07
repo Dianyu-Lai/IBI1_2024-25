69d85c78c49ef98378968d370c0db2f50d9d0e9a
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-04-07 16:20:27 +0800

    improved

--- practical7/tata_box.py
+++ practical7/tata_box.py
@@ -1,16 +1,15 @@
 import re
 with open('tata_genes.fa', 'r') as input, open('tata_box.fa', 'w') as output:
-    current_gene=None#initialize a variable to store the current gene name
+    current_gene=[]#initialize a variable to store the current gene name
     current_sequence=[]#initialize an empty list to store the current sequence
     tata_pattern=re.compile(r'TATA[AT]A[AT]')#define a regular expression pattern to match TATWAW
     for line in input: #iterate through each line in the file
-        if re.search(r'^>\w+mRNA',line):#check gene name line
+        if re.search(r'^>\w+_mRNA',line):#check gene name line
             if current_gene and current_sequence:#if the current gene and sequence are not empty
-                full_sequence=''.join(line.strip() for line in current_sequence)#join the sequence lines
+                full_sequence=''.join(line.strip() for seq in current_sequence)#join the sequence lines
                 if tata_pattern.search(full_sequence):#check if the sequence includes TATWAW
-                    output.write(current_gene[0])
-                    output.write('\n')
-                    output.write(' '.join(current_sequence))
+                    output.write(f'{current_gene}\n')
+                    output.writelines(current_sequence)#write the sequence to the output file                   
             current_gene=re.findall(r'(>\w+)_mRNA',line)#extract the gene name
             current_sequence=[]
         else:#for seqenece line
@@ -20,6 +19,7 @@ with open('tata_genes.fa', 'r') as input, open('tata_box.fa', 'w') as output:
     if current_gene and current_sequence:#if the current gene and sequence are not empty
         full_sequence=''.join(line.strip() for line in current_sequence)#join the sequence lines
         if tata_pattern.search(full_sequence):#check if the sequence includes TATWAW
-            output.write(current_gene[0])
-            output.write('\n')
-            output.write(' '.join(current_sequence))
+            gene_match=re.search(r'>(\w+)_mRNA',current_gene[0])#extract the gene name
+            if gene_match:#if the gene name is found
+                output.write(f'>{current_gene[0]}\n')
+                output.writelines(current_sequence)
