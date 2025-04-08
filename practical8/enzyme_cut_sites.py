6ef8cc1539e76775e5910028d2549eb122804c55
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-04-08 16:37:18 +0800

    practical8

--- practical8/enzyme_cut_sites.py
+++ practical8/enzyme_cut_sites.py
@@ -1,27 +1,26 @@
 def enzyme_cut_sites(dna_seq, recognised_seq):
-    dna_seq=input('enter the DNA sequence to be cut: ')
-    for nucleotide in dna_seq:
+    dna_seq=input('enter the DNA sequence to be cut: ')#input the DNA sequence to be cut
+    for nucleotide in dna_seq:#check if the DNA sequence contains only A, T, G, C
         if nucleotide not in ['A','T','G','C']:
-            raise ValueError('DNA sequence should only contain A, T, G, C')
+            raise ValueError('DNA sequence should only contain A, T, G, C')#raise an error if the DNA sequence contains non-canonical nucleotides
     else:
         recognised_seq=input('enter the recognised sequence: ')
-        for nucleotide in recognised_seq:
+        for nucleotide in recognised_seq:#check if the sequence recognised by restriction enzyme contains only A, T, G, C
             if nucleotide not in ['A','T','G','C']:
                 raise ValueError('sequence recognised by restriction enzyme should only contain A, T, G, C')
         else:
-            cut_sizes=[]
+            cut_sites=[]#create an empty list to store the cut sites
             enzyme_len=len(recognised_seq)
             for i in range(len(dna_seq)-enzyme_len+1):
                 if dna_seq[i:i+enzyme_len]==recognised_seq:
-                    cut_sizes.append(i+1)
-            if cut_sizes==[]:
+                    cut_sites.append(i+1)
+            if cut_sites==[]:
                 print('no cut site found')
             else:
-                return cut_sizes
+                return cut_sites#return the list of cut sites
 
 if __name__=='__main__':
     #appropiate example:
-    
     try:
         test_dna_seq='ATCGATCGATCGATCG'
         test_recognised_seq='CGAT'
