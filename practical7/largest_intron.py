7f0dd696ab94a4a008c5d135169dd5b051457ec8
Dianyu-Lai <dianyu.24@intl.zju.edu.cn>
2025-05-20 16:15:31 +0800

    use the right symbol in the regular expression

--- practical7/largest_intron.py
+++ practical7/largest_intron.py
@@ -1,9 +1,9 @@
 import re   #import library
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA' #the sequence
-intron = re.findall(r'GT.*？AG',seq)#find the largest intron
+intron = re.findall(r'GT.*?AG',seq)#find the largest intron
 # if any introns found, find the longest one
-if introns:
-    longest_intron = max(introns, key=len)
+if intron:
+    longest_intron = max(intron, key=len)
     print("The largest intron is:", longest_intron)
     print("Length:", len(longest_intron))
 else:
