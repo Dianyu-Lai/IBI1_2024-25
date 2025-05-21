dc829d7b1d0f71c7d12b08822d3460237cc85ddb
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-05-21 20:29:34 +0800

    fix the repetition error and make it neater

--- practical7/tata_genes.py
+++ practical7/tata_genes.py
@@ -1,25 +1,42 @@
 import re
-with open('tata_genes.fa', 'r') as input, open('tata_box.fa', 'w') as output:
-    current_gene=[]#initialize a variable to store the current gene name
-    current_sequence=[]#initialize an empty list to store the current sequence
-    tata_pattern=re.compile(r'TATA[AT]A[AT]')#define a regular expression pattern to match TATWAW
-    for line in input: #iterate through each line in the file
-        if re.search(r'^>\w+_mRNA',line):#check gene name line
-            if current_gene and current_sequence:#if the current gene and sequence are not empty
-                full_sequence=''.join(line.strip() for seq in current_sequence)#join the sequence lines
-                if tata_pattern.search(full_sequence):#check if the sequence includes TATWAW
-                    output.write(f'{current_gene}\n')
-                    output.writelines(current_sequence)#write the sequence to the output file                   
-            current_gene=re.findall(r'(>\w+)_mRNA',line)#extract the gene name
-            current_sequence=[]
-        else:#for seqenece line
+
+with open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa', 'r') as input, open('tata_genes.fa', 'w') as output:
+    #initialize lists to store the current gene name and sequence
+    current_gene = []
+    current_sequence = []
+    tata_pattern = re.compile(r'TATA[AT]A[AT]')#define a regular expression pattern to match TATAWAW
+    gene_name = re.compile(r'^>.*?gene:(\S+) ')
+
+    #iterate through each line in the file to get the gene name and sequence
+    for line in input:
+        #check gene name line
+        if gene_name.search(line):
+            #if the current gene and sequence are not empty
+            if current_gene and current_sequence:
+                #join the sequence lines
+                full_sequence = ''.join(line.strip() for line in current_sequence)
+
+                #check if the sequence includes TATAWAW
+                if tata_pattern.search(full_sequence):
+                    #write the sequence to the output file
+                    output.write(f'>{current_gene[0]}\n')
+                    output.writelines(current_sequence)         
+
+            current_gene = gene_name.findall(line) #extract the gene name
+            current_sequence = [] #clear the previous sequence
+
+        else:
             current_sequence.append(line)  #add the sequence line to the current sequence list
 
     #write the last gene and sequence to the output file
-    if current_gene and current_sequence:#if the current gene and sequence are not empty
-        full_sequence=''.join(line.strip() for line in current_sequence)#join the sequence lines
-        if tata_pattern.search(full_sequence):#check if the sequence includes TATWAW
-            gene_match=re.search(r'>(\w+)_mRNA',current_gene[0])#extract the gene name
-            if gene_match:#if the gene name is found
-                output.write(f'>{current_gene[0]}\n')
-                output.writelines(current_sequence)
+    if current_gene and current_sequence:
+        #join the sequence lines
+        full_sequence = ''.join(line.strip() for line in current_sequence)
+
+        if tata_pattern.search(full_sequence):
+            output.write(f'>{current_gene[0]}\n')
+            output.writelines(current_sequence)
+
+    else:
+        current_sequence.append(line)
+        
