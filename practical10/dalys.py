6c567401b910677071ec7290c7ccbbfb295b793e
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-05-24 17:10:14 +0800

    add some comments and spaces in the code and change the code line of new task to 29-39

--- practical10/dalys.py
+++ practical10/dalys.py
@@ -5,29 +5,30 @@ import numpy as np
 
 dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
 
-print(dalys_data.iloc[0:10,2])# the year is 1999, and DALYs is 82624.94 in Afghanistan
+print(dalys_data.iloc[0:10,2]) #the year is 1999, and DALYs is 82624.94 in Afghanistan
 
-is_1990=dalys_data['Year']==1990
-print(dalys_data.loc[is_1990,'DALYs'])
+is_1990 = dalys_data['Year'] == 1990
+print(dalys_data.loc[is_1990,'DALYs']) #
 
-uk=dalys_data.loc[dalys_data.Entity=='United Kingdom',['DALYs','Year']]
+uk=dalys_data.loc[dalys_data.Entity == 'United Kingdom',['DALYs','Year']]
 uk_mean=uk['DALYs'].mean()
 print(uk_mean)
-france=dalys_data.loc[dalys_data.Entity=='France',['DALYs','Year']]
-france_mean=france['DALYs'].mean()
+france = dalys_data.loc[dalys_data.Entity == 'France',['DALYs','Year']]
+france_mean = france['DALYs'].mean()
 print(france_mean)
 #the mean of UK is larger than the mean of France
 
-plt.plot(uk.Year,uk.DALYs,'b+-',label='UK')
+plt.plot(uk.Year,uk.DALYs,'b+-',label = 'UK')
 plt.title('DALYs over time in UK')
 plt.xlabel('Year')
 plt.ylabel('DALYs in UK')
 plt.legend()
 plt.xticks(uk.Year,rotation=-75)
 plt.show()
+
 #answer to question.txt
-low_income=dalys_data.loc[dalys_data.Entity=='World Bank Low Income',['DALYs','Year']]
-high_income=dalys_data.loc[dalys_data.Entity=='World Bank High Income',['DALYs','Year']]
+low_income = dalys_data.loc[dalys_data.Entity == 'World Bank Low Income',['DALYs','Year']]
+high_income = dalys_data.loc[dalys_data.Entity == 'World Bank High Income',['DALYs','Year']]
 plt.plot(low_income.Year,low_income.DALYs,'ro-',label='low income countries')
 plt.plot(high_income.Year,high_income.DALYs,'bo-',label='high income countries')
 plt.xlabel('Year')
