29f37ef2b95a67a6b8a201512fa996b7aff23e3a
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-05-24 16:26:49 +0800

    define a new function to check the validity of DNA seq and change the code of reporting the errors

--- practical8/enzyme_cut_sites.py
+++ practical8/enzyme_cut_sites.py
@@ -1,36 +1,39 @@
+#define a function to check if the nucleotides in the DNA sequence only contain A, T, G, C
+def check_nucleotides(seq):
+    for nucleotide in seq:
+        if nucleotide not in 'ATGC':
+            return False
+    return True
+            
 def enzyme_cut_sites(dna_seq, recognised_seq):
-    dna_seq=input('enter the DNA sequence to be cut: ')#input the DNA sequence to be cut
-    for nucleotide in dna_seq:#check if the DNA sequence contains only A, T, G, C
-        if nucleotide not in ['A','T','G','C']:
-            raise ValueError('DNA sequence should only contain A, T, G, C')#raise an error if the DNA sequence contains non-canonical nucleotides
-    else:
-        recognised_seq=input('enter the recognised sequence: ')
-        for nucleotide in recognised_seq:#check if the sequence recognised by restriction enzyme contains only A, T, G, C
-            if nucleotide not in ['A','T','G','C']:
-                raise ValueError('sequence recognised by restriction enzyme should only contain A, T, G, C')
-        else:
-            cut_sites=[]#create an empty list to store the cut sites
-            enzyme_len=len(recognised_seq)
-            for i in range(len(dna_seq)-enzyme_len+1):
-                if dna_seq[i:i+enzyme_len]==recognised_seq:
-                    cut_sites.append(i+1)
-            if cut_sites==[]:
-                print('no cut site found')
-            else:
-                return cut_sites#return the list of cut sites
+    
+    #check if the sequences contain only A, T, G, C
+    if check_nucleotides(dna_seq) == False or check_nucleotides(recognised_seq) == False:
+        print('invalid nucleotides in the sequence')
+    
+    #create an empty list to store the cut sites
+    cut_sites = []
+    for i in range(len(dna_seq) - len(recognised_seq) + 1):
+        if dna_seq[i:i + len(recognised_seq)] == recognised_seq:
+            cut_sites.append(i+1)
+
+    return cut_sites
 
 if __name__=='__main__':
+    #input the DNA sequence to be cut
+    dna_seq = input('enter the DNA sequence to be cut: ').strip().upper()
+
+    #if the DNA sequence is valid, ask for the input of restriction enzyme recognised sequence
+    recognised_seq = input('enter the recognised sequence: ').strip().upper()
+
+    sites = enzyme_cut_sites(dna_seq, recognised_seq)
+
+    if sites:
+        print(f"Cut sites found at positions: {sites}")
+    else:
+        print("No cut sites found")
+
     #appropiate example:
-    try:
-        test_dna_seq='ATCGATCGATCGATCG'
-        test_recognised_seq='CGAT'
-        sites=enzyme_cut_sites(test_dna_seq, test_recognised_seq)
-        print(f'cut sites found at positions: {sites}')
-    except ValueError as e:
-        print(e)
-    # example with non-canonical nucleotides
-    try:
-        non_canonical_seq='ATCGANCGATCGATCG'
-        sites=enzyme_cut_sites(non_canonical_seq, test_recognised_seq)
-    except ValueError as e:
-        print(e)
+    #dna_seq = 'ATCGATCGATCGATCG'
+    #recognised_seq = 'CGAT'
+    #expected output: cut sites found at positions: [3, 7, 11]
