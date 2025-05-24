c83b04585c6d59da53abd86075f8518755e111b9
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-05-24 15:57:30 +0800

    delete a useless input in drug_calculator.py, and add a new example to patients_record.py

--- practical8/drug_calculator.py
+++ practical8/drug_calculator.py
@@ -1,29 +1,25 @@
 def drug_calculator():
-    age=input('enter your age in years: ')
-    if age >18:
-        print('age should be below 18 years')
+
+    weigh = int(input('enter your weight in kg: '))
+    #check if weight is in the range
+    if weigh > 100 or weigh < 10:
+        print('weight of children should be between 10 and 100 kg')
     else:
-        weigh=input('enter your weight in kg: ')
-        if weigh>100 or weigh<10:
-            print('weight of children should be between 10 and 100 kg')
+        dosage=15 * weigh
+        strength = input('enter your drug strength between 120mg/5mL and 250mg/5mL: ') #please notice that mg/mL the "L" is capitalized
+        #check if the strength is in the range
+        if strength not in ['120mg/5mL','250mg/5mL']:
+            print('strength should be 120mg/5mL or 250mg/5mL')
         else:
-            dosage=15*weigh
-            print(f'drug dosage is {dosage}mg')
-            strength=input('enter your drug strength between 120mg/5mL and 250mg/5mL: ')
-            if strength not in ['120mg/5mL','250mg/5mL']:
-                print('strength should be 120mg/5mL or 250mg/5mL')
+            if strength=='120mg/5mL':
+                volume=dosage*5/120
+                print(f'volume of solution required is {volume}mL')
             else:
-                if strength=='120mg/5mL':
-                    volume=dosage*5/120
-                    print(f'volume of solution required is {volume}mL')
-                else:
-                    volume=dosage*5/250
-                    print(f'volume of solution required is {volume}mL')
+                volume=dosage*5/250
+                print(f'volume of solution required is {volume}mL')
 drug_calculator()
-#directly input age, weight, strength to use this code
 #example:
-#enter your age in years: 15
 #enter your weight in kg: 60
-#enter your drug strength between 120mg/5mL and 250mg/5mL: 120mg/5mL
+#enter your drug strength between 120mg/5mL and 250mg/5mL: 120mg/5mL (please notice that mg/mL the "L" is capitalized)
 #drug dosage is 900mg
-#volume of solution required is 18.0mL
+#volume of solution required is 37.5mL
--- practical8/patients_record.py
+++ practical8/patients_record.py
@@ -1,11 +1,16 @@
 class patients:
-    def __init__(self, name, age , admission_date, medical_history):#create a class with attributes
+    #create a class with attributes
+    def __init__(self, name, age , admission_date, medical_history):
         self.name=name
         self.age=age
         self.admission_date=admission_date
         self.medical_history=medical_history
+    #create a method to print the details of the patient
     def print_details(self):
         print(f"Name: {self.name}, Age: {self.age}, Admission Date: {self.admission_date}, Medical History: {self.medical_history}")
 #create an example
-patient1=patients('Sha Rui',18,'2025-01-01','too_smart')
+patient1 = patients('Sha Rui',18,'2025-01-01','too_smart')
 patient1.print_details()
+#another Example:
+#patient2 = patients('Johnson',60,'2079-05-15','diabetes')
+#patient2.print_details()
