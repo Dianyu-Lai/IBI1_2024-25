d17dddc4e5ff6a008573e6afedac10038ff8129d
Dianyu Lai <dianyu.24@intl.zhu.edu.cn>
2025-05-25 16:50:11 +0800

    replace the function output from [print] to [return]

--- practical8/drug_calculator.py
+++ practical8/drug_calculator.py
@@ -1,23 +1,26 @@
-def drug_calculator():
+def drug_calculator(weigh, strength):
 
-    weigh = int(input('enter your weight in kg: '))
     #check if weight is in the range
     if weigh > 100 or weigh < 10:
         print('weight of children should be between 10 and 100 kg')
     else:
         dosage=15 * weigh
-        strength = input('enter your drug strength between 120mg/5mL and 250mg/5mL: ') #please notice that mg/mL the "L" is capitalized
         #check if the strength is in the range
         if strength not in ['120mg/5mL','250mg/5mL']:
             print('strength should be 120mg/5mL or 250mg/5mL')
         else:
-            if strength=='120mg/5mL':
+            if strength == '120mg/5mL':
                 volume=dosage*5/120
-                print(f'volume of solution required is {volume}mL')
+
             else:
                 volume=dosage*5/250
-                print(f'volume of solution required is {volume}mL')
-drug_calculator()
+            
+            return volume
+        
+weigh = int(input('enter your weight in kg: '))
+strength = input('enter your drug strength between 120mg/5mL and 250mg/5mL: ') #please notice that mg/mL the "L" is capitalized
+volume = drug_calculator(weigh,strength)
+print(f"the volume of solution required is {volume}mL")
 #example:
 #enter your weight in kg: 60
 #enter your drug strength between 120mg/5mL and 250mg/5mL: 120mg/5mL (please notice that mg/mL the "L" is capitalized)
